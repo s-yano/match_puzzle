e4cf8437ff3ddc3260d5a3800492278016733bb8
YANO Satoru <s-yano@pb.jp.nec.com>
2015-08-28 17:10:30 +0900

    refactoring

--- solver.rb
+++ solver.rb
@@ -8,11 +8,12 @@ class Solver
     @num = n
     @result = []
     @count = 0
-    @area = VirtualMatrix.new(0)
+    @area = VirtualMatrix.new
   end
 
   def print_area
     @area.print_matrix(0 => "  ", 1 => "＼", 2 => "／")
+    # @area.print_matrix
   end
 
   def solve!
@@ -23,9 +24,9 @@ class Solver
   def same_pattern?
     a1 = @area
     4.times do
-      a1 = a1.rotate.conv
+      a1 = a1.rotate
       return true if @result.include?(a1.to_s)
-      a2 = a1.mirror.conv
+      a2 = a1.mirror
       return true if @result.include?(a2.to_s)
     end
     @result << @area.to_s
@@ -33,14 +34,15 @@ class Solver
   end
 
   def sub(x, y, m, dep)
+    # puts "#{x}, #{y}, #{m}, #{dep}"
     if @num == dep
       return if same_pattern?
-      print_area
-      puts "--------------------"
       @count += 1
+      puts "-------------------- (#{@count})"
+      print_area
       return
     end
-    return if @area[x, y] != 0
+    return if @area[x, y]
     @area[x, y] = m
     if m == 1
       sub(x-1, y-1, 1, dep+1)
@@ -56,5 +58,9 @@ class Solver
     @area.del(x, y)
   end
 end
+
+# (1..6).each do |i|
+#   puts "#{i}本 #{Solver.new(i).solve!}通り"
+# end
 
 p Solver.new(5).solve!
--- virtual_matrix.rb
+++ virtual_matrix.rb
@@ -3,61 +3,44 @@
 
 # 仮想的な無限格子空間
 class VirtualMatrix
-  def initialize(default_val=nil)
-    @default_val = default_val
-    @pos = Hash.new(@default_val)
-    @delim = ":"
+  Pos = Struct.new(:x, :y, :v)
+  def initialize
+    @pos = Hash.new
   end
   def buildkey(x, y)
-    "#{x}#{@delim}#{y}"
+    "#{x}/#{y}"
   end
   alias _ buildkey
-  def sepkey(k)
-    k.split(@delim).map(&:to_i)
-  end
   def [](x, y)
-    @pos[_(x, y)]
+    p = @pos[_(x, y)]
+    p.nil? ? nil : p.v
   end
-  def []=(x, y, n)
-    @pos[_(x, y)] = n
-    # puts "#{_(x, y)} = #{n}"
+  def []=(x, y, v)
+    @pos[_(x, y)] = Pos.new(x, y, v)
   end
-  # def set(x, y)
-  #   self[x, y] = 1
-  # end
   def del(x, y)
-    @pos.delete _(x, y)
-  end
-  def vacuum
-    @pos.each {|k, v| @pos.delete(k) if v == @default_val || v.nil? }
-    self
+    @pos.delete(_(x, y))
   end
   def clear
     @pos.clear
   end
   def dump
     @pos.each do |k, v|
-      x, y = sepkey(k)
-      puts "#{x}, #{y} -> #{v}"
+      puts "#{v.x}, #{v.y} -> #{v.v}"
     end
   end
-  def print_matrix(hash={})
+  def print_matrix(hash={0 => "  ", 1 => "＼", 2 => "／"})
     range = box
-    # puts "(#{range[:xmin]},#{range[:ymin]})"
     (range[:ymin]..range[:ymax]).each do |y|
       (range[:xmin]..range[:xmax]).each do |x|
-        v = self[x, y]
+        v = self[x, y] || 0
         print "#{hash[v]||v}"
       end
       puts
     end
-    # puts "(#{range[:xmax]},#{range[:ymax]})"
   end
   def positions
-    @pos.keys.map do |k|
-      x, y = sepkey(k)
-      [x.to_i, y.to_i]
-    end
+    @pos.values.map {|v| [v.x, v.y] }
   end
 
   def box
@@ -68,45 +51,23 @@ class VirtualMatrix
       xs << x
       ys << y
     end
-    # [[xs.min, ys.min], [xs.max, ys.max]]
     { xmin: xs.min, ymin: ys.min, xmax: xs.max, ymax: ys.max }
   end
 
   def rotate
-    des = VirtualMatrix.new(@default_val)
-    @pos.each do |k,v|
-      x, y = sepkey(k)
-      des[-y, x] = v
-    end
-    des
-  end
-  alias rotate_clockwise rotate
-
-  def rotate_ccw
-    des = VirtualMatrix.new(@default_val)
-    @pos.each do |k,v|
-      x, y = sepkey(k)
-      des[y, -x] = v
+    des = VirtualMatrix.new
+    @pos.each do |_,v|
+      des[-v.y, v.x] = 3 - v.v
     end
     des
   end
 
   def mirror
-    des = VirtualMatrix.new(@default_val)
-    @pos.each do |k,v|
-      x, y = sepkey(k)
-      des[-x, y] = v
+    des = VirtualMatrix.new
+    @pos.each do |_,v|
+      des[-v.x, v.y] = 3 - v.v
     end
     des
-  end
-
-  def dup
-    des = VirtualMatrix.new(@default_val)
-    @pos.each do |k,v|
-      x, y = sepkey(k)
-      des[x, y] = v
-    end
-    des#.vacuum
   end
 
   def serialize
@@ -115,31 +76,11 @@ class VirtualMatrix
     (range[:ymin]..range[:ymax]).each do |y|
       xs = []
       (range[:xmin]..range[:xmax]).each do |x|
-        xs << self[x, y]
+        xs << (self[x, y] || 0)
       end
       des << xs.join
     end
     des.join("/")
   end
   alias to_s serialize
-
-  def normalize
-    des = VirtualMatrix.new(@default_val)
-    range = box
-    (range[:ymin]..range[:ymax]).each.with_index do |y,yi|
-      (range[:xmin]..range[:xmax]).each.with_index do |x,xi|
-        des[xi, yi] = self[x, y]
-      end
-    end
-    des
-  end
-
-  def conv(h={1 => 2, 2 => 1})
-    des = VirtualMatrix.new(@default_val)
-    @pos.each do |k,v|
-      x, y = sepkey(k)
-      des[x, y] = h.keys.include?(v) ? h[v] : v
-    end
-    des
-  end
 end
